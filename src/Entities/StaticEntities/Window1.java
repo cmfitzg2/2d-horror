e4029b182beb9bf1a224717918e7ef73f891e678
cfitzgerald <gelatinous.octahedron@gmail.com>
2020-12-24 11:41:28 -0500

    adding some windows

---
+++ src/Entities/StaticEntities/Window1.java
@@ -0,0 +1,59 @@
+package Entities.StaticEntities;
+
+import Graphics.Assets;
+import Variables.Handler;
+import Worlds.World;
+
+import java.awt.*;
+
+public class Window1 extends StaticEntity {
+
+    public Window1(Handler handler, float x, float y, int width, int height, String uniqueName) {
+        super(handler, x, y, width, height, uniqueName);
+        bounds.x = 0;
+        bounds.y = 0;
+        bounds.width = width;
+        bounds.height = height;
+    }
+
+    @Override
+    public void preRender(Graphics g) {
+
+    }
+
+    @Override
+    public void postRender(Graphics g) {
+
+    }
+
+    @Override
+    public void finalRender(Graphics g) {
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.drawImage(Assets.windowOne, (int) (x - handler.getGameCamera().getxOffset()),
+                (int) (y - handler.getGameCamera().getyOffset()), width, height, null);
+    }
+
+    @Override
+    public void die() {
+        handler.getActiveWorld().getEntityManager().removeEntity(this);
+    }
+
+    @Override
+    public void interactedWith() {
+
+    }
+
+    @Override
+    public boolean isInteracting() {
+        return isInteracting;
+    }
+}
--- src/Worlds/World3.java
+++ src/Worlds/World3.java
@@ -2,6 +2,9 @@ package Worlds;
 
 import Entities.StaticEntities.Door1;
 import Entities.StaticEntities.Hole;
+import Entities.StaticEntities.Window1;
+import Graphics.Assets;
+import Tiles.Tile;
 import Variables.Handler;
 
 public class World3 extends World {
@@ -18,7 +21,11 @@ public class World3 extends World {
     @Override
     protected void addEntities() {
         entityManager.addEntity(new Hole(handler, 420, 420, 64, 64, "Hole 3", handler.getWorldManager().getWorld(2), 100, 100));
-        entityManager.addEntity(new Door1(handler, 960, 576, 64, 96, "Door1", handler.getWorldManager().getWorld(2), 900, 900, true, true));
+        entityManager.addEntity(new Door1(handler, 960, 9 * Tile.TILEHEIGHT, 64, 96, "Door1", handler.getWorldManager().getWorld(2), 900, 900, true, true));
+        entityManager.addEntity(new Window1(handler, 858, 9 * Tile.TILEHEIGHT - 12, 64, 88, "Window1-1"));
+        entityManager.addEntity(new Window1(handler, 770, 9 * Tile.TILEHEIGHT - 12, 64, 88, "Window1-2"));
+        entityManager.addEntity(new Window1(handler, 1062, 9 * Tile.TILEHEIGHT - 12, 64, 88, "Window1-3"));
+        entityManager.addEntity(new Window1(handler, 1164, 9 * Tile.TILEHEIGHT - 12, 64, 88, "Window1-4"));
     }
 
     @Override
